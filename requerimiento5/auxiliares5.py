ff5343e40d32c0ad8b1936eb55ef50c3066a36d7
OscarCampohermoso <oscarcampohermoso8@gmail.com>
2023-11-28 22:57:25 -0300

    req5

--- requerimiento5/auxiliares5.py
+++ requerimiento5/auxiliares5.py
@@ -4,6 +4,13 @@ import pandas as pd
 import time
 
 def obtener_datos_trimestrales(mm, aa, empresa_busqueda='CENCOSUD S.A.'):
+    """
+    Obtiene los datos trimestrales de una empresa en un mes y año específicos.
+    :param mm: Mes en formato de dos dígitos (03, ..., 12)
+    :param aa: Año en formato de cuatro dígitos ('2013', '2014', ..., '2023')
+    :param empresa_busqueda: Nombre de la empresa a buscar
+    :return: DataFrame con los datos trimestrales de la empresa
+    """
     url = f'https://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs={mm}&aa_ifrs={aa}'
     headers = {
         "User-Agent": "Mozilla/5.0 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
@@ -29,14 +36,13 @@ def obtener_datos_trimestrales(mm, aa, empresa_busqueda='CENCOSUD S.A.'):
             if not (enlace_primer_envio and enlace_ultimo_envio and enlace_razon_social):
                 continue
 
-            # Extraer texto utilizando get_text() y strip=True para limpiar espacios
-            fecha_primer_envio = enlace_primer_envio.get_text(strip=True)
-            fecha_ultimo_envio = enlace_ultimo_envio.get_text(strip=True)
+            fecha_primer_envio = enlace_primer_envio.get_text()
+            fecha_ultimo_envio = enlace_ultimo_envio.get_text()
             razon_social = enlace_razon_social.get_text(strip=True)
             
             # Si la razón social coincide con la empresa buscada, añadir los datos al DataFrame
             if empresa_busqueda.lower() == razon_social.lower():
-                trimestre_financiero = f'{aa}-{mm}-30' if mm != '03' else f'{aa}-03-31'
+                trimestre_financiero = f'{aa}-{mm}-31' if mm in ['03', '12'] else f'{aa}-{mm}-30'
                 datos.append({
                     'Trimestre_Estado_Financiero': trimestre_financiero,
                     'Fecha_Primer_envio': fecha_primer_envio,
@@ -46,7 +52,13 @@ def obtener_datos_trimestrales(mm, aa, empresa_busqueda='CENCOSUD S.A.'):
     return pd.DataFrame(datos)
 
 
-def guardar_datos(df, nombre_db='datos_eeff.db', nombre_tabla='EstadosFinancieros'):
+def guardar_datos(df, nombre_db='datos_eeff.db', nombre_tabla='FECHAS_EEFF'):
+    """
+    Guarda los datos de un DataFrame en una base de datos SQL.
+    :param df: DataFrame con los datos a guardar
+    :param nombre_db: Nombre de la base de datos
+    :param nombre_tabla: Nombre de la tabla
+    """
     # Verifica si el DataFrame está vacío
     if df.empty:
         print("El DataFrame está vacío. No se guardará en la base de datos.")
@@ -59,6 +71,6 @@ def guardar_datos(df, nombre_db='datos_eeff.db', nombre_tabla='EstadosFinanciero
             print(f"Falta la columna necesaria: {columna}")
             return
     
-    # Guardar el DataFrame en una base de datos SQL utilizando la función 'to_sql'
+    # Guardar el DataFrame en una base de datos SQL
     conexion = f'sqlite:///{nombre_db}'
     df.to_sql(nombre_tabla, conexion, if_exists='append', index=False)
