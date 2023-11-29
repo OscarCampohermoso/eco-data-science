f06b0818d82c78dcae2f4c262e67fb6c54e580c4
OscarCampohermoso <oscarcampohermoso8@gmail.com>
2023-11-29 18:21:23 -0300

    req5 finalizado

--- requerimiento5/auxiliares5.py
+++ requerimiento5/auxiliares5.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+
 def obtener_datos_trimestrales(mm, aa, empresa_busqueda='CENCOSUD S.A.'):
     """
     Obtiene los datos trimestrales de una empresa en un mes y año específicos.
@@ -16,11 +17,12 @@ def obtener_datos_trimestrales(mm, aa, empresa_busqueda='CENCOSUD S.A.'):
         "User-Agent": "Mozilla/5.0 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
 
-    time.sleep(5)  # Pausa para prevenir bloqueos por hacer peticiones muy seguidas
+    # Pausa para prevenir bloqueos por hacer peticiones muy seguidas
+    time.sleep(5)
     response = requests.get(url, headers=headers)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'html.parser')
-    
+
     datos = []
     # Encuentra todas las filas de la tabla en la página
     for fila in soup.find_all('tr'):
@@ -39,16 +41,17 @@ def obtener_datos_trimestrales(mm, aa, empresa_busqueda='CENCOSUD S.A.'):
             fecha_primer_envio = enlace_primer_envio.get_text()
             fecha_ultimo_envio = enlace_ultimo_envio.get_text()
             razon_social = enlace_razon_social.get_text(strip=True)
-            
+
             # Si la razón social coincide con la empresa buscada, añadir los datos al DataFrame
             if empresa_busqueda.lower() == razon_social.lower():
-                trimestre_financiero = f'{aa}-{mm}-31' if mm in ['03', '12'] else f'{aa}-{mm}-30'
+                trimestre_financiero = f'{aa}-{mm}-31' if mm in [
+                    '03', '12'] else f'{aa}-{mm}-30'
                 datos.append({
                     'Trimestre_Estado_Financiero': trimestre_financiero,
                     'Fecha_Primer_envio': fecha_primer_envio,
                     'Fecha_ultimo_envio': fecha_ultimo_envio
                 })
-    
+
     return pd.DataFrame(datos)
 
 
@@ -63,14 +66,15 @@ def guardar_datos(df, nombre_db='datos_eeff.db', nombre_tabla='FECHAS_EEFF'):
     if df.empty:
         print("El DataFrame está vacío. No se guardará en la base de datos.")
         return
-    
+
     # Verifica que las columnas necesarias están en el DataFrame
-    columnas_necesarias = ['Trimestre_Estado_Financiero', 'Fecha_Primer_envio', 'Fecha_ultimo_envio']
+    columnas_necesarias = ['Trimestre_Estado_Financiero',
+                           'Fecha_Primer_envio', 'Fecha_ultimo_envio']
     for columna in columnas_necesarias:
         if columna not in df.columns:
             print(f"Falta la columna necesaria: {columna}")
             return
-    
+
     # Guardar el DataFrame en una base de datos SQL
     conexion = f'sqlite:///{nombre_db}'
     df.to_sql(nombre_tabla, conexion, if_exists='append', index=False)
